4e6fd6dc4d8d813b87e41bc1c76d76549a682f32
yonier999 <yonierjulian999@gmail.com>
2021-05-23 10:03:47 -0500

    se agrego cards con links en vista admin y se creo sus respectivos archivos php

--- administrador/indexAdmin.php
+++ administrador/indexAdmin.php
@@ -5,8 +5,42 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vista Principal Estudiante</title>
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Administrador</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarColor01">
+      
+      
+    </div>
+  </div>
+</nav>
+<section><br>
+    <center><h1>Advanced School</h1></center>
+    <div class="menu">
+        <a href="crearUsuario.php"><div class="alert alert-dismissible alert-info targeta">
+            <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+            <strong>Crear Usuario</strong> 
+        </div></a>
+        <a href="materia.php"><div class="alert alert-dismissible alert-info targeta">
+            <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+            <strong>Materia</strong> 
+        </div></a>
+        <a href="notas.php"><div class="alert alert-dismissible alert-info targeta">
+         <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+            <strong>Modificar Notas</strong> 
+        </div></a>
+        
+    </div>
+
+</section>
     
 </body>
 </html>
